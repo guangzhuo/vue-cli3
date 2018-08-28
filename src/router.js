af9b93a021b674961c6972adf338c214358f44ae
GZ <949404079@qq.com>
2018-08-28 12:12:45 +0800

    404

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ Vue.use(Router);
 
 const Index = resolve => require(['@/views/index'],resolve)
 const Test =  resolve => require(['@/views/test'],resolve)
+const NotFour =  resolve => require(['@/views/404'],resolve)
 export default new Router({
   mode :'history',
   routes: [
@@ -16,6 +17,11 @@ export default new Router({
       path: '/test',
       name: 'test',
       component: Test
-    }
+    },
+      {
+          path: '*',
+          name: '404',
+          component: NotFour
+      }
   ]
 });
